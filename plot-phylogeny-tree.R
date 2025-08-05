dfb4106ef0150b014f3a2e3c76c8b1e17f2ffde6
Victor Gonzalez <gonram.vm@gmail.com>
2025-08-05 20:40:43 +0300

    General debugging

--- plot-phylogeny-tree.R
+++ plot-phylogeny-tree.R
@@ -11,18 +11,30 @@ library(phytools)
 # -----------------------------
 # Config paths
 # -----------------------------
-input_file <- "OG0000195_clean2_Myo2_mafft_80trim"
-phyl_dir <- "phylogeny_analysis"
-pipeline_dir <- file.path(phyl_dir, "puhti")
-tree_dir <- file.path(pipeline_dir, "iqtree_files")
+datasets <- c("OG0000195","Char_septins")
+iterations <- c("","_clean","_clean2")
+trimmings <- c("","_80trim")
+outgroups <- c("","_GtpA","_Myo2","_hMyo2")
+rooting_branches <- c("","Dicdi-GtpA","Sacce1-Myo2","Homsa-Myo2")
+names(rooting_branches) <- outgroups
+
+
+dataset <- datasets[1]
+iteration <- iterations[3]
+trimming <- trimmings[2]
+outgroup <- outgroups[3]
+rooting_branch <- rooting_branches[outgroup]
+
+
+input_file <- paste0(dataset,iteration,outgroup,"_mafft",trimming)
+data_dir <- "local_data"
+phyl_dir <- file.path(data_dir, 'phylogeny_analysis')
+tree_dir <- file.path(phyl_dir, 'iqtree_files')
 
 tree_file <- file.path(tree_dir, paste0(input_file, ".treefile"))
 
-tree_file <- file.path(tree_dir,"Char_septins_mafft_septin.treefile")
-tree_file <- file.path(tree_dir,"Char_septins_mafft_80trim.treefile")
-
 septin_file <- file.path(phyl_dir, "Septins.csv")
-tax_file <- "proteome_list_orthofinder.csv"
+tax_file <- file.path(data_dir, "proteome_list_orthofinder.csv")
 tax_outgroup_file <- file.path(phyl_dir, "outgroup_phylogeny.csv")
 output_tree <- file.path(tree_dir, paste0(input_file, ".pdf"))
 output_node_tree <- file.path(tree_dir, paste0(input_file, "_nodes.pdf"))
@@ -37,16 +49,14 @@ tree <- read.tree(tree_file)
 stopifnot(!is.null(tree), length(tree$tip.label) > 0)
 
 # Root by priority
-# if ("Sacce1-Myo2" %in% tree$tip.label) {
-#   tree <- root(tree, outgroup = "Sacce1-Myo2", resolve.root = TRUE)
-# } else if ("Dicdi-GtpA" %in% tree$tip.label) {
-#   tree <- root(tree, outgroup = "Dicdi-GtpA", resolve.root = TRUE)
-# } else {
-#   tree <- midpoint.root(tree)
-# }
-ggtree(tree)
-tree <- root(tree, node = 1250)
-tree <- root(tree, outgroup = "Sacce1-Myo2", resolve.root = TRUE)
+if (is.na(rooting_branch)) {
+  tree <- midpoint.root(tree)
+} else {
+  tree <- root(tree, outgroup = rooting_branch, resolve.root = TRUE)
+}
+
+ggtree(tree) + geom_tiplab(align=TRUE, linesize=.01)
+
 # -----------------------------
 # Load and process metadata
 # -----------------------------
